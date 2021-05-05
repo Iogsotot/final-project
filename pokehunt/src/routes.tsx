5f955825158655ef17bc0f41e0e94c0eceae4e5b
Iogsotot <ajustusa@gmail.com>
2021-05-05 23:17:40 +0300

    feat: add basic file structure & routes

---
+++ pokehunt/src/routes.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { BrowserRouter , Switch, Route, Redirect } from 'react-router-dom';
+import MainPage from './components/MainPage';
+
+export const Router: FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <MainPage/>
+        </Route>
+
+        {/* <Route path='/' exact>
+          <PokemonCard/>
+        </Route> */}
+
+        {/* <Route path='/' exact>
+          <MyCollection/>
+        </Route> */}
+
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  )
+}
