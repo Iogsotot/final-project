c0d6c5b28de76a400bc46ecc2b9e01f9955297a8
Iogsotot <ajustusa@gmail.com>
2021-05-07 00:00:51 +0300

    feat: add appDescription (320px)

--- frontend/src/components/AppDescription/AppDescription.tsx
+++ frontend/src/components/AppDescription/AppDescription.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react';
+import './appDescription.scss';
 
 const AppDescription: FC = () => (
-  <section className='app-description'>How it works?</section>
+  <section className='app-description'>
+    <h2 className='title'>How it works?</h2>
+    <p className="text">
+      If you want to catch a Pokemon, then just select the Pokemon
+      you like and click the "Catch" button. Hooray! now it is in your
+      Pokemon collection and you can always find it in the "My Collection" tab.
+    </p>
+    <img src="/imgs/how-it-works.png" alt="" />
+  </section>
 );
 
 export default AppDescription;
