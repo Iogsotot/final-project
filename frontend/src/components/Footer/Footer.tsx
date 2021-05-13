c8ae873234a1c917267f1d4ee591ab8835481df3
Iogsotot <ajustusa@gmail.com>
2021-05-13 21:40:38 +0300

    feat: add styles for footer (320px)

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,10 +1,37 @@
 import { FC } from 'react';
+import logo from '../../assets/logo.png';
+import './footer.scss';
 
-const Footer: FC = () => {
-  const any = 1;
-  return (
-    <footer></footer>
-  );
-};
+const Footer: FC = () => (
+  <footer>
+    <div className="wrapper">
+      <div className="nav-group">
+        <div className="logo-group">
+          <div className="logo__title">POKE</div>
+          <div className="logo__subtitle">
+              hunt
+            <img src={logo} alt="" className='logo__img'/>
+          </div>
+        </div>
+
+        <nav>
+          <ul className='footer__nav'>
+            <li className="nav__item">Home</li>
+            <li className="nav__item">My Collection</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="line" />
+
+      <div className="copyright">
+        <div className="copyright__design">© Design based on the work of No   ora Sagnayeva</div>
+        <div className="copyright__developer">© App made by <b>Anna Justus</b></div>
+      </div>
+
+    </div>
+
+  </footer>
+);
 
 export default Footer;
